e1b52bbedc4174c3adafb9494bb188b81311905e
Joe Gilbert <joegilbertdev@outlook.com>
2024-10-16 23:48:38 +0100

    works

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -6,8 +6,8 @@ interface CanvasProps {
 }
 
 export const Canvas = ({ canvasRef }: CanvasProps) => {
-    const GRID_SIZE = 4;
-    const RESOLUTION = 128;
+    const GRID_SIZE = 16;
+    const RESOLUTION = ;
     const COLOR_SCALE = 250;
     const CANVAS_WIDTH = 640;
     const pixel_size = CANVAS_WIDTH / RESOLUTION;
@@ -19,8 +19,11 @@ export const Canvas = ({ canvasRef }: CanvasProps) => {
         if (ctx !== null) {
             for (let y = 0; y < GRID_SIZE; y += num_pixels / GRID_SIZE) {
                 for (let x = 0; x < GRID_SIZE; x += num_pixels / GRID_SIZE) {
-                    const v = getPerlinNoise(x, y) * COLOR_SCALE;
-                   ctx.fillStyle = 'hsl(' + v + ',50%,50%)';
+                    const v = getPerlinNoise(x, y);
+                    const alpha = Math.max(0, v * COLOR_SCALE);
+                    //ctx.fillStyle = 'hsl(' + v + ',50%,50%)';
+                    ctx.globalAlpha = alpha;
+                    ctx.fillStyle = 'black';
                     ctx.fillRect(
                         x / GRID_SIZE * CANVAS_WIDTH,
                         y / GRID_SIZE * CANVAS_WIDTH,
@@ -28,6 +31,7 @@ export const Canvas = ({ canvasRef }: CanvasProps) => {
                         pixel_size
                     );
                 }
+                ctx.globalAlpha = 1;
             }
         }
     }, [canvasRef, num_pixels, pixel_size]);
