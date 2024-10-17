bbd4f4a5fb17ee838e7b44b743334ee023da8689
Joe Gilbert <joegilbertdev@outlook.com>
2024-10-17 17:52:16 +0100

    reset seed button

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -3,23 +3,27 @@ import { getPerlinNoise } from "../lib/perlinNoise";
 
 interface CanvasProps {
     canvasRef: MutableRefObject<HTMLCanvasElement | null>;
+    seed: number;
 }
 
-export const Canvas = ({ canvasRef }: CanvasProps) => {
+export const Canvas = ({ canvasRef, seed }: CanvasProps) => {
     const GRID_SIZE = 16;
     const RESOLUTION = 64;
     const CANVAS_WIDTH = 640;
+    const CANVAS_HEIGHT = 320;
     const pixel_size = CANVAS_WIDTH / RESOLUTION;
     const num_pixels = GRID_SIZE / RESOLUTION;
-    const SEED = Math.floor(Math.random() * 90000) + 10000;
 
     useEffect(() => {
         const current = canvasRef.current;
         const ctx = current!.getContext('2d');
+
         if (ctx !== null) {
+            // clear for each redraw
+            ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
             for (let y = 0; y < GRID_SIZE; y += num_pixels / GRID_SIZE) {
                 for (let x = 0; x < GRID_SIZE; x += num_pixels / GRID_SIZE) {
-                    const v = getPerlinNoise(x, y, SEED);
+                    const v = getPerlinNoise(x, y, seed);
                     //ctx.fillStyle = 'hsl(' + v + ',50%,50%)';
                     ctx.globalAlpha = Math.max(0, v);
                     /*
@@ -28,7 +32,7 @@ export const Canvas = ({ canvasRef }: CanvasProps) => {
                     ctx.fillStyle = 'black';
                     ctx.fillRect(
                         x / GRID_SIZE * CANVAS_WIDTH,
-                        y / GRID_SIZE * CANVAS_WIDTH,
+                        y / GRID_SIZE * CANVAS_HEIGHT,
                         pixel_size,
                         pixel_size
                     );
@@ -36,7 +40,7 @@ export const Canvas = ({ canvasRef }: CanvasProps) => {
                 ctx.globalAlpha = 1;
             }
         }
-    }, [canvasRef, num_pixels, pixel_size, SEED]);
+    }, [canvasRef, num_pixels, pixel_size, seed]);
 
 
     return (
--- src/components/MapAndControls.tsx
+++ src/components/MapAndControls.tsx
@@ -1,12 +1,19 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { Canvas } from "./Canvas";
 
 export const MapAndControls = () => {
     const canvasRef = useRef<HTMLCanvasElement | null>(null)
+    const [seed, setSeed] = useState(Math.floor(Math.random() * 90000) + 10000);
+
+    const onNewSeed = () => setSeed(Math.floor(Math.random() * 90000) + 10000);
 
     return (
         <>
-        <Canvas canvasRef={canvasRef}/>
+        <Canvas canvasRef={canvasRef} seed={seed}/>
+
+        <div>
+            <button onClick={onNewSeed}>Reset seed</button>
+        </div>
         </>
     )
 };
